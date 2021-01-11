e0e22d589316dbd13939508442a609ce8449d73f
alenj <alenjojo252001@gmail.com>
2021-01-11 20:29:10 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I'm the best");
+            var name = "Alen";
+            Console.WriteLine($"My name is Kinda {name}");
+        }
+    }
+}
